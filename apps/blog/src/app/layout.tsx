356b8bc7e4659f9f04b04c5ae429c8b2b22e0ed2
Carla Goncalves <goncalves@prisma.io>
2026-03-04 09:52:12 +0000

    Update landing page for blog

--- apps/blog/src/app/layout.tsx
+++ apps/blog/src/app/layout.tsx
@@ -27,7 +27,7 @@ export default function Layout({ children }: LayoutProps<"/">) {
           crossOrigin="anonymous"
         ></Script>
       </head>
-      <body className="flex flex-col min-h-screen">
+      <body className="flex flex-col min-h-screen bg-blog pt-24">
         <RootProvider>{children}</RootProvider>
       </body>
     </html>
