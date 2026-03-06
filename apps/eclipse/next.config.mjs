d808dcf617b630fc1581014fb128a5e5d026ec0e
Carla Goncalves <goncalves@prisma.io>
2026-03-06 16:13:08 +0000

    Add functioning newsletter

--- apps/eclipse/next.config.mjs
+++ apps/eclipse/next.config.mjs
@@ -1,13 +1,12 @@
-import { createMDX } from 'fumadocs-mdx/next';
+import { createMDX } from "fumadocs-mdx/next";
 
 const withMDX = createMDX();
 
 /** @type {import('next').NextConfig} */
 const config = {
   reactStrictMode: true,
-  output: 'export',
   images: { unoptimized: true },
-  transpilePackages: ['@prisma-docs/eclipse'],
+  transpilePackages: ["@prisma-docs/eclipse"],
 };
 
 export default withMDX(config);
